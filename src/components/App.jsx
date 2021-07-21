a0cd16f123fa0ee3a1378dd01f9dd2cb562db098
ArkarKyawSwar <arkar.kswar@gmail.com>
2021-07-21 16:56:36 +0630

    Add navigation between five main pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import BottomNav from "./BottomNav";
-import SearchBar from "./SearchBar"
+
+import SuppliesPage from "./SuppliesPage";
+import ChecklistsPage from "./ChecklistsPage";
+import HomePage from "./HomePage";
+import CampsPage from "./CampsPage";
+import VolunteersPage from "./VolunteersPage";
 
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
@@ -8,8 +15,13 @@ import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 function App() {
   return (
     <div>
-      <SearchBar />
-      <BottomNav />
+    <Route exact path="/" component={HomePage} />
+    <Route exact path="/supplies" component={SuppliesPage} />
+    <Route exact path="/checklists" component={ChecklistsPage} />
+    <Route exact path="/camps" component={CampsPage} />
+    <Route exact path="/volunteers" component={VolunteersPage} />
+
+    <BottomNav />
     </div>
   );
 }
---
+++ src/components/CampsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function CampsPage() {
+  return (
+    <div> 
+        <h1>Camps page</h1>
+    </div>
+  );
+}
+
+export default CampsPage;
